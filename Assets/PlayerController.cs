d1b150c4d52ddd51c291a31a075b7b15eb57bd1e
worldduck <tonyskh0803@gmail.com>
2022-09-14 17:38:50 +0900

    20220914
    플레이어 스크립트-Input클래스 사용한 입력감지

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -16,6 +16,28 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            //위쪽 방향키 입력이 감지된 경우 z 방향 힘 주기
+            playerRigidbody.AddForce(0f, 0f, speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            //아래쪽 방향키 입력이 감지된 경우 -z 방향 힘 주기
+            playerRigidbody.AddForce(0f, 0f, -speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            //오른쪽 방향키 입력이 감지된 경우 x 방향 힘 주기
+            playerRigidbody.AddForce(speed, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            //왼쪽 방향키 입력이 감지된 경우 -x 방향 힘 주기
+            playerRigidbody.AddForce(-speed, 0f, 0f);
+        }
     }
 }
