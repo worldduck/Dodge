8d6b84faf0900fc56b8a5e15202d1e802da0bc93
worldduck <tonyskh0803@gmail.com>
2022-09-19 12:36:01 +0900

    20220919
    Bullet 오브젝트 ->프리팹으로 만들기, Bullet 스크립트 준비

---
+++ Assets/Bullet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // 이동에 사용할 리지드바디 컴포넌트
+    private Rigidbody bulletRigidbody;
+    // 탄알 이동 속력
+    public float speed = 8f;
+    void Start()
+    {
+
+    }
+}
