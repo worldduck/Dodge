836a8e8f467986e3dc0bd0ba464c2b631929f399
worldduck <tonyskh0803@gmail.com>
2022-09-21 12:39:47 +0900

    20220921_01
    총알 생성기 복제, 회전 스크립트 생성

--- Assets/02.Scripts/BulletSpawner.cs
+++ Assets/02.Scripts/BulletSpawner.cs
@@ -6,7 +6,7 @@ public class BulletSpawner : MonoBehaviour
 {
     public GameObject bulletPrefab; // 생성할 탄알의 원본 프리팹
     public float spawnRateMin = 0.5f; // 최소 생성 주기
-    public float spawnRatwMax = 3f; // 최대 생성 주기
+    public float spawnRateMax = 3f; // 최대 생성 주기
 
     private Transform target; // 발사할 대상
     private float spawnRate; // 생성 주기
@@ -17,7 +17,7 @@ public class BulletSpawner : MonoBehaviour
         // 최근 생성 이후의 누적 시간을 0으로 초기화
         timeAfterSpawn = 0f;
         // 탄알 생성 간격을 spawnRateMin 과 spawnRateMax 사이에서 랜덤 지정
-        spawnRate = Random.Range(spawnRateMin, spawnRatwMax);
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
         // PlayerController 컴포넌트를 가진 게임 오브젝트를 찾아 조준 대상으로 설정
         target = FindObjectOfType<PlayerController>().transform;
     }
@@ -41,7 +41,7 @@ public class BulletSpawner : MonoBehaviour
             // 생성된 bullet 게임 오브젝트의 정면 방향이 target을 향하도록 회전
             bullet.transform.LookAt(target);
             // 다음번 생성 간격을 spawnRateMin, spawnRateMin 사이에서 랜덤 지정
-            spawnRate = Random.Range(spawnRateMin, spawnRatwMax);
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
         }
     }
 }
