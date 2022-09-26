877bb3ce38d5a66cdf7c80c9ff4cbbb4f3fed05d
worldduck <tonyskh0803@gmail.com>
2022-09-26 17:39:08 +0900

    20220926
    테스트씬 마우스 순간이동 스크립트

--- Assets/02.Scripts/CatFootSpawner.cs
+++ Assets/02.Scripts/CatFootSpawner.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletSpawner : MonoBehaviour
+public class CatFootSpawner : MonoBehaviour
 {
-    public GameObject bulletPrefab; // 생성할 탄알의 원본 프리팹
+    public GameObject catFootBulletPrefab; // 생성할 탄알의 원본 프리팹
     public float spawnRateMin = 0.5f; // 최소 생성 주기
     public float spawnRateMax = 3f; // 최대 생성 주기
 
@@ -12,6 +12,7 @@ public class BulletSpawner : MonoBehaviour
     private float spawnRate; // 생성 주기
     private float timeAfterSpawn; // 최근 생성 시점에서 지난 시간
 
+    // Start is called before the first frame update
     void Start()
     {
         // 최근 생성 이후의 누적 시간을 0으로 초기화
@@ -19,10 +20,10 @@ public class BulletSpawner : MonoBehaviour
         // 탄알 생성 간격을 spawnRateMin 과 spawnRateMax 사이에서 랜덤 지정
         spawnRate = Random.Range(spawnRateMin, spawnRateMax);
         // PlayerController 컴포넌트를 가진 게임 오브젝트를 찾아 조준 대상으로 설정
-        target = FindObjectOfType<PlayerController>().transform;
+        target = FindObjectOfType<MouseController>().transform;
     }
 
-
+    // Update is called once per frame
     void Update()
     {
         // timeAfterSpawn 갱신
@@ -37,7 +38,7 @@ public class BulletSpawner : MonoBehaviour
             // bulletPrefab 복제본을
             // transform.position 위치와 transform.rotation 회전으로 생성
             GameObject bullet
-                = Instantiate(bulletPrefab, transform.position, transform.rotation);
+                = Instantiate(catFootBulletPrefab, transform.position, transform.rotation);
             // 생성된 bullet 게임 오브젝트의 정면 방향이 target을 향하도록 회전
             bullet.transform.LookAt(target);
             // 다음번 생성 간격을 spawnRateMin, spawnRateMin 사이에서 랜덤 지정
